760a27205952972d96e9d4cb4dad2c32e7e2a5a2
skillpoint <>
2024-01-26 15:25:19 +0100

    First layout idea

---
+++ cna/docusaurus.config.js
@@ -0,0 +1,141 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'CNA',
+  tagline: 'CNCF is cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Cloud Native Austria', // Usually your GitHub org/user name.
+  projectName: 'cna-website', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'de',
+    locales: ['de', 'en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Cloud Native Austria',
+        logo: {
+          alt: 'CNA Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/cloud-native-austria',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'light',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Meetup',
+                href: 'https://www.meetup.com/cncf-graz/',
+              },
+              {
+                label: 'CNCF Community Graz',
+                href: 'https://community.cncf.io/graz/',
+              },
+              {
+                label: 'Slack (CNCF #graz channel)',
+                href: 'https://communityinviter.com/apps/cloud-native/cncf',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/cloud-native-austria'
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Cloud Native Austria`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ cna/src/components/Locations/index.js
@@ -0,0 +1,49 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const LocationList = [
+  {
+    location: 'Graz',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+  },
+  {
+    location: 'Wien',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  },
+  {
+    location: 'Salzburg',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  },
+  {
+    location: 'Linz',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  },
+];
+
+function Location({Svg, location}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.locationSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{location}</Heading>
+      </div>
+    </div>
+  );
+}
+
+export default function Locations() {
+  return (
+    <section className={styles.locations}>
+      <div className="container">
+        <div className="row">
+          {LocationList.map((props, idx) => (
+            <Location key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
