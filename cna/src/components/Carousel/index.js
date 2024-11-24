4f841931eb82d6748fca3ae74976f2759c2645b6
Daniel Drack <daniel.drack@fullstacks.eu>
2024-11-24 15:14:11 +0100

    fix: updated carousel style

--- cna/src/components/Carousel/index.js
+++ cna/src/components/Carousel/index.js
@@ -1,6 +1,6 @@
 import styles from './styles.module.css';
-import "../../../node_modules/slick-carousel/slick/slick.css";
-import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 import Slider from "react-slick";
 import useBaseUrl from '@docusaurus/useBaseUrl';
@@ -8,35 +8,67 @@ import Heading from '@theme/Heading';
 
 function Slide({logo, headline, text, url}) {
     return (
-        <a className={styles.slide} href={url} target="_blank">
+        <div className={styles.slide}>
             <div className={styles.slideIcon}>
                 <img className={styles.slideSVG} src={useBaseUrl(logo)}/>
             </div>
             <div className={styles.slideText}>
                 <div>
-                    <Heading as="h1">{headline}</Heading>
+                    <a href={url} target="_blank">
+                        <Heading as="h2">{headline}</Heading>
+                    </a>
                     {text}
                 </div>
             </div>
-        </a>
+        </div>
     );
 }
 
 export default function Carousel() {
-    var settings = {
+    const settings = {
         dots: true,
-        fade: true,
         infinite: true,
         autoplay: true,
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 3,
+        centerMode: true,
+        centerPadding: '60px',
         slidesToScroll: 1,
-        waitForAnimate: false
+        waitForAnimate: false,
+        adaptiveHeight: false,
     };
+
+
+
     return (
         <div className={styles.base}>
             <div className="container">
                 <Slider {...settings}>
+                    <Slide
+                        logo='/img/graz.svg'
+                        headline='November Meetup Graz'
+                        text='Come join us at the November meetup of the CNCF Graz community!'
+                        url='graz/20241120'/>
+                    <Slide
+                        logo='/img/graz.svg'
+                        headline='Dezember Meetup Graz'
+                        text='DEZEMBER!'
+                        url='graz/20241120'/>
+                    <Slide
+                        logo='/img/graz.svg'
+                        headline='Jänner Meetup Graz'
+                        text='JÄNNER'
+                        url='graz/20241120'/>
+                    <Slide
+                        logo='/img/graz.svg'
+                        headline='Februar Meetup Graz'
+                        text='Februar!'
+                        url='graz/20241120'/>
+                    <Slide
+                        logo='/img/graz.svg'
+                        headline='November Meetup Graz'
+                        text='Come join us at the November meetup of the CNCF Graz community!'
+                        url='graz/20241120'/>
                     <Slide
                         logo='/img/graz.svg'
                         headline='November Meetup Graz'
