5cd1ba0d67d348749b3d58c8c3fa5bf18a40a9c9
Daniel Drack <daniel.drack@fullstacks.eu>
2024-12-01 15:04:42 +0100

    fix: make carousel responsive

--- cna/src/components/Carousel/index.js
+++ cna/src/components/Carousel/index.js
@@ -32,6 +32,24 @@ export default function Carousel() {
         autoplay: true,
         speed: 500,
         slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ],
         centerMode: true,
         centerPadding: '60px',
         slidesToScroll: 1,
